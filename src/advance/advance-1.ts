6ab935b32bb6960f35a032469af85fa439339899
vibhor <vibgitcode27@gmail.com>
2023-12-14 14:05:15 +0530

    changed files structure and added advance-1.ts

---
+++ src/advance/advance-1.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main()
+{
+    const user = await prisma.user.findMany({
+        where: {
+            email : {
+                endsWith : "@gmail.com"
+            },
+            posts: {
+                some: {
+                    published : true
+                }
+            }
+        },
+        include: {
+            posts: {
+                where: {
+                    published : true
+                }
+            }
+        }
+    })
+}
---
+++ src/basic/delete-unpublished-posts.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main()
+{
+    await prisma.post.deleteMany({
+        where : {
+            published : false
+        }
+    })
+}
+
+main().then(async () => {
+    await prisma.$disconnect();
+}).catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect();
+    process.exit(1);
+})
+
+// For any specific user
+
+// async function main()
+// {
+//     await prisma.user.update({
+//         where: {
+//             id : 1
+//         },
+//         data : {
+//             posts : {
+//                 deleteMany : {
+//                     published : false
+//                 }
+//             }
+//         }
+//     })
+// }
